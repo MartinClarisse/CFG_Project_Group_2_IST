5b97b04d370f380fe9f8f18c7ce96e864eb7900c
Clarisse <clarisse.dg.martin@gmail.com>
2024-08-22 11:59:27 +0100

    Cleaning up the python and adding annotations

--- website/db.py
+++ website/db.py
@@ -1,48 +1,37 @@
 import mysql.connector
-from flask import current_app, g #g is special object in flask that stores informatio that's being handled.
-from flask import Flask
-# ------------------------------------------------------------
-
-# Test Example, connection working
-# cursor = mydb.cursor()
-# query = "INSERT INTO Trips (trip_name, start_date)  VALUES (%s,%s);"
-# data = ("Test", "2024-08-21")
-# cursor.execute(query, data)
-# mydb.commit()
-# cursor.close()
-# mydb.close()
 
 # ------------------------------------------------------------
 
-def get_mydb():
-    if 'my_db' not in g:
-        mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="9731",
-        database="BudgetBuddy"
+# This file has functions for connecting to the database, then querying and closing it.
+# This means the functions can be called in the models file without needing to be repeated each time.
+# For example, you would just import * functions then just define the query and pass it through the query_db function.
+
+# ------------------------------------------------------------
+# This function connects to the database
+# ❗NB: The password needs to be changed for programmer's own MySQL log in to connect.
+
+def get_db():
+    return mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="9731",
+    database="BudgetBuddy"
     )
-        return g.my_db
 
-def close_my_db(e=None):
-    my_db = g.pop('my_db', None)
-    if my_db is not None:
-        my_db.close()
-
-def query_my_db(query, args=(), one=False):
-    db = get_mydb()
-    cursor = db.cursor(dictionary=True)
+# ------------------------------------------------------------
+# This function allows you to query the database by passing in the query (SELECTS AND INSERTS ETC).
+def query_db(query, args=()):
+    db = get_db()
+    cursor = db.cursor()
     cursor.execute(query, args)
     rv = cursor.fetchall()
     cursor.close()
-    return (rv[0] if rv else None) if one else rv
-
-
-
+    db.close()
+    return rv
 
 # ------------------------------------------------------------
+# Quick check it works... (it does hooray!, This can be properly tested in the testing file once everything working)
+# query = "SELECT * FROM Trips"
+# results = query_db(query)
+# print(results)
 
-# class Trips(db.Model)
-#     id = db.Column(db.Integer, primary_key=True)
-
-# --------------------------------
--- website/models.py
+++ website/models.py
@@ -1,8 +1,56 @@
-from db import get_mydb
+from db import get_db, query_db
+import hashlib # Needed for authentication
+# ------------------------------------------------------------
 
-class Trip_inputs:
-    @staticmethod
-    my_db = get_mydb()
-    cursor = my_db.cursor()
-    cu
+# This is a file which puts all the code which queries SQL.
+# So once the variables are taken from the inputs in the views, they can be passed into functions here and returned to views.
+# I'm thinking this will probably end up wth more files, like one for each page I imagine?
+# And to save on replicating code, the actual db function gets called from db so only a query needs to be passed.
+# Here I am unsure if it needs classes or can just be functions?
+
+
+# ------------------------------------------------------------
+# How to authenticate the user login inputs from html
+class Authentication:
+    def __init__ (self, username, password):
+        self.username = username
+        self.password = password
+
+    def check_user_exists(self):
+        query = "SELECT * FROM  Authentication WHERE username = %s" # Used args instead of f string to avoid SQL injection attacks
+        args = (self.username,)
+        results = query_db(query, args)
+        return bool(results) # better than if clause for True or False
+
+    def check_password_match(self):
+        hashed_password = hashlib.md5(self.password.encode()).hexdigest() # This matches the encryption which happens when a user is created with the query.
+        query = "SELECT * FROM  Authentication WHERE username = %s AND PWD = %s"
+        args = (self.username, hashed_password,)
+        results = query_db(query, args)
+        return bool(results)
+
+
+
+# 'Testing' the Class to make sure authentication works properly'
+# Rather than this, this would need to then use the boolean values in if statements on view to determine wether to move on to dashboard or raise error.
+
+# Using our Dud
+username = 'thedud'
+password = 'password'
+
+authenticate = Authentication(username, password)
+
+
+
+# Checking if the user exists
+if authenticate.check_user_exists():
+    print("User exists")
+else:
+    print("User does not exist")
+
+# Checking if the password matches
+if authenticate.check_password_match():
+    print("Password matches")
+else:
+    print("Password does not match")
 
